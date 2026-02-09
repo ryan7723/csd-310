9e12b6e93ffcaedcbfd847771a14dd64081068de
Ryan <ryan.772@hotmail.comgit config --global user.name Ryangit config --global user.email ryan.772@hotmail.com>
2026-02-09 03:58:18 -0500

    Module 5 assignment

---
+++ module-5/mysql_test.py
@@ -0,0 +1,37 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+from dotenv import dotenv_values
+
+
+secrets = dotenv_values(".env")
+
+config = {
+    "user": secrets["USER"],
+    "password": secrets["PASSWORD"],
+    "host": secrets["HOST"],
+    "database": secrets["DATABASE"],
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print(
+        "\nDatabase user {} connected to MySQL on host {} with database {}"
+        .format(config["user"], config["host"], config["database"])
+    )
+
+    input("\nPress any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The supplied username or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("The specified database does not exist")
+    else:
+        print(err)
+
+finally:
+    if 'db' in locals() and db.is_connected():
+        db.close()
